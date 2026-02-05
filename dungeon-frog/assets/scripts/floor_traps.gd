fe4b5a98e07d1f7e70b0c5da6e09f9e14905ca72
Jesús Estor <animaleco@users.noreply.github.com>
2026-02-05 19:52:48 +0100

    Add falling trap system

--- dungeon-frog/assets/scripts/floor_traps.gd
+++ dungeon-frog/assets/scripts/floor_traps.gd
@@ -3,6 +3,7 @@ extends Node2D
 var animation_controller: AnimationController
 var trigger: Trigger
 
+
 func _ready() -> void:
 	animation_controller = $AnimationController
 	trigger = $Trigger
--- dungeon-frog/assets/scripts/trap.gd
+++
@@ -1,7 +0,0 @@
-extends RigidBody2D
-
-@export var raycast: RayCast2D
-
-func _physics_process(_delta: float) -> void:
-	if raycast.get_collider():
-		freeze = false
